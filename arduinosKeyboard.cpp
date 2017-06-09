b0451d2c34b14bd6ab82f57a2131aa9d600d1c55
Felix Deierlein <felix.deierlein@swisscom.com>
2017-06-09 20:01:17 +0200

    Replaced keypad implementation with the Keypad.h lib and adapted it my PINs

--- arduinosKeyboard.cpp
+++ arduinosKeyboard.cpp
@@ -1,174 +1,43 @@
+//#include <Key.h>
+#include <Keypad.h>
+
 #include "arduinos.h"
+//TODO: we do not have a 4x4 keyboard...
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+byte rowPins[ROWS] = {D2, D3, D4, D5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {D6, D7, D8}; //connect to the column pinouts of the keypad
+
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 /*
  * Configure ports used for the keyboard matrix
  */
 void setupKeyboard() {
-    // Keyboard column
-    pinMode(A0, INPUT); digitalWrite(A0, HIGH);
-    pinMode(A1, INPUT); digitalWrite(A1, HIGH);
-    pinMode(A2, INPUT); digitalWrite(A2, HIGH);
-    pinMode(A3, INPUT); digitalWrite(A3, HIGH);
-    // Keyboard row
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-}
-
-/*
- * When keyboard scan is done, set all rows low
- */
-void setAllOutputLow() {
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-}
-
-/*
- * Detect which column that has been asserted
- */
-int getColumn() {
-    int columns[] = {A0, A1, A2, A3};
-    for (int i=0; i<(sizeof(columns)/sizeof(int)); i++) {
-        int a = columns[i];
-        if (digitalRead(a) == 0) {
-            return a;
-        }
-    }
-    return 0;
-}  
-
-/*
- * Detect which row that has been asserted
- */
-int getRow(int column) {
-    int row = 0;
-    for (int r=2; r<=5; r++) {
-        digitalWrite(r, HIGH); 
-        delay(1); 
-        int c = digitalRead(column);
-        if (c == 1) {
-            row = r;
-            break;
-        }
-    }  
-    return row;
-}
-
-/*
- * Based on row & column, determine which key has been pressed
- */
-char getKey(int row, int column) {
-    char keyPressed = 0;
-    switch (row) {
-        case 2:
-            switch (column) {
-                case A0: keyPressed = 'A'; break;
-                case A1: keyPressed = '3'; break;
-                case A2: keyPressed = '2'; break;
-                case A3: keyPressed = '1'; break;
-                default:  break;
-            }
-            break;
-        case 3:
-            switch (column) {
-                case A0: keyPressed = 'B'; break;
-                case A1: keyPressed = '6'; break;
-                case A2: keyPressed = '5'; break;
-                case A3: keyPressed = '4'; break;
-                default: break;
-            }
-            break;
-        case 4:
-            switch (column) {
-                case A0: keyPressed = 'C'; break;
-                case A1: keyPressed = '9'; break;
-                case A2: keyPressed = '8'; break;
-                case A3: keyPressed = '7'; break;
-                default:  break;
-            }
-            break;
-        case 5:
-            switch (column) {
-                case A0: keyPressed = 'D'; break;
-                case A1: keyPressed = '#'; break;
-                case A2: keyPressed = '0'; break;
-                case A3: keyPressed = '*'; break;
-                default:  break;
-            }
-            break;
-        default:
-            break;
-    }
-    return keyPressed;
-}
-
-/*
- * Perform a single keyboard scan
- */
-char scanKeyboard() {
-    setAllOutputLow();
-    char k = 0;
-    int c = getColumn();
-    if (c != 0) {
-        int r = getRow(c);
-        if (r != 0) {
-            k = getKey(r, c);
-        }
-    }
-    return k;
-}
-
-/*
- * Debouncing of keys. The same key must be detected twice to be accepted. 
- * 
- * So far there has not been a need for a delay between the two detections, nor for more than 2 detections
- */
-char debouncedKey() {
-    char key1 = scanKeyboard();
-    if (key1 != 0) {
-        char key2 = scanKeyboard();
-        if (key1 != key2) {
-            key1 = 0;
-        }
-    }
-    return key1;
+  
 }
 
 /*
  * Main keyboard scan which is performed on every loop
  */
 char loopKeyboard() {
-    static char kPrev = 0;
-    
-    char k = debouncedKey();
-    if (k != kPrev) {
-        // Something changed
-        if (k == 0) {
-            // Key released
+    char key = keypad.getKey();
+
 #ifdef DEBUG
-            Serial.print(F(" - released:"));
-            Serial.println(kPrev);
-#endif
-            kPrev = k;
-        } else {
-            // Key held down
-            if (kPrev == 0) {
-#ifdef DEBUG
-                Serial.print(F("Key pressed:"));
-                Serial.print(k);
-#endif
-              kPrev = k;
-            } else {
-                k = 0; // Ignore the keypress
-            }
-        }
-    } else {
-        k = 0; // Ignore the keypress
+    if(key) {
+      Serial.print(F("Key pressed:"));
+      Serial.print(key);
     }
-    return k;
+#endif
+    
+    return key;
 }
 
 
